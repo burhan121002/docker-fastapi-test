230312f0e548797a4f6d998181005e6e3d17b88e
burhan121002 <106872441+burhan121002@users.noreply.github.com>
2024-07-04 00:05:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from typing import List
 from fastapi import FastAPI
-
 from app import services
 from app.schema import UserIn, BaseResponse, UserListOut
 
 app = FastAPI()
-
 
 @app.get("/")
 async def index():
@@ -14,7 +12,6 @@ async def index():
     """
     return {"message": "Hello from FastAPI ;)"}
 
-
 @app.post("/users", response_model=BaseResponse)
 async def user_create(user: UserIn):
     """
@@ -22,10 +19,10 @@ async def user_create(user: UserIn):
     """
     try:
         services.add_userdata(user.dict())
-    except:
+    except Exception as e:
+        print(f"Error adding user: {str(e)}")  # Add logging for debugging
         return {"success": False}
     return {"success": True}
-
 
 @app.get("/users", response_model=UserListOut)
 async def get_users():
